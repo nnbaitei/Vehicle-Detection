c59d3fc27ae19e2fa22d85fa25071f5482d5c533
nnbaitei <nnbaitei@gmail.com>
2024-10-03 03:20:21 +0700

    fix preprocess lp image and data in csv

--- detect_lpr.py
+++ detect_lpr.py
@@ -6,18 +6,18 @@ import numpy as np
 import pandas as pd
 from ultralytics import YOLO
 
-# pre-trained YOLOv8 model for car recognition
+# Pre-trained YOLOv8 model for car recognition
 coco_model = YOLO('yolov8n.pt')
-# yolov8 model trained to detect license plates
+# YOLOv8 model trained to detect license plates
 np_model = YOLO(r'runs\detect\license_plate_detection\weights\best.pt')
 
-# read in test video paths
+# Read in test video paths
 videos = glob(r'D:\Y4\สหกิจ\Vehicle-Detection\car_detect.avi')
 
 # Set up EasyOCR reader
 reader = easyocr.Reader(['th'], gpu=True)
 
-# Create folder for saving license plate images
+# Create folders for saving license plate and vehicle images
 output_folder = './detected_license_plates'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
@@ -31,36 +31,68 @@ def read_license_plate(license_plate_crop):
         return text, score
     return None, None
 
-# Function to save license plate images
-def save_license_plate_image(frame_number, track_id, plate_img):
-    output_path = os.path.join(output_folder, f"frame_{frame_number}_track_{track_id}.jpg")
+# Function to save vehicle images (if not already saved)
+def save_vehicle_image(frame_number, track_id, vehicle_img, saved_track_ids):
+    if track_id not in saved_track_ids:
+        output_path = os.path.join(output_folder, f"vehicle_{track_id}.jpg")
+        cv.imwrite(output_path, vehicle_img)
+        saved_track_ids.add(track_id)
+        return output_path  # Return the image file path for CSV
+    return None  # If image was already saved, return None
+
+# Function to save license plate images (if detected)
+def save_license_plate_image(track_id, plate_img):
+    output_path = os.path.join(output_folder, f"license_plate_{track_id}.jpg")
     cv.imwrite(output_path, plate_img)
+    return output_path  # Return the image file path for CSV
 
 # Function to write the results to CSV
 def write_csv(results, output_file):
     with open(output_file, 'w', encoding='utf-8') as f:
-        f.write('frame_number,track_id,class,x1,y1,x2,y2,Number_license_plate\n')
+        f.write('frame_number,track_id,class,x1,y1,x2,y2,Number_license_plate,Vehicle_image_path,License_plate_image_path\n')
         for frame_number, objects in results.items():
             for track_id, data in objects.items():
                 class_name = data['vehicle']['class_name']
                 bbox_obj = data['vehicle']['bbox']
                 license_plate = data.get('license_plate', None)
+                vehicle_image_path = data['vehicle']['image_path']
+                license_plate_image_path = data['license_plate'].get('image_path', None)
 
                 if license_plate:
-                    f.write('{},{},{},{},{},{},{},{}\n'.format(
+                    f.write('{},{},{},{},{},{},{},{},{}\n'.format(
                         frame_number, track_id, class_name, 
                         bbox_obj[0], bbox_obj[1], bbox_obj[2], bbox_obj[3], 
-                        license_plate['number']
+                        license_plate['number'], vehicle_image_path, license_plate_image_path
                     ))
                 else:
                     f.write('{},{},{},{},{},{},{},{}\n'.format(
                         frame_number, track_id, class_name, 
                         bbox_obj[0], bbox_obj[1], bbox_obj[2], bbox_obj[3], 
-                        'No License Plate'
+                        'No License Plate', vehicle_image_path, 'No License Plate Image'
                     ))
+
+def preprocess_image(img):
+    scale_factor = 20  # Increase image size by 20 times
+    resized_image = cv.resize(img, (0, 0), fx=scale_factor, fy=scale_factor, interpolation=cv.INTER_LANCZOS4)
+
+    # Sharpening the image
+    sharpening_kernel = np.array([[0, -1, 0],
+                                [-1, 5.5, -1],
+                                [0, -1, 0]])
+    sharpened_image = cv.filter2D(resized_image, -1, sharpening_kernel)
+
+    # Convert to grayscale and remove noise using bilateral filter
+    gray = cv.cvtColor(sharpened_image, cv.COLOR_BGR2GRAY)
+    bfilter = cv.bilateralFilter(gray, 1, 1, 1)
+
+    # Increase contrast with CLAHE
+    clahe = cv.createCLAHE(clipLimit=10, tileGridSize=(8,8))
+    contrast_image = clahe.apply(bfilter)
+    return contrast_image
 
 # Initialize results dictionary
 results = {}
+saved_track_ids = set()
 
 # Read video by index
 video = cv.VideoCapture(videos[0])
@@ -74,12 +106,14 @@ output_video_path = 'output_video.avi'
 fourcc = cv.VideoWriter_fourcc(*'XVID')  # Codec for video writing
 video_writer = cv.VideoWriter(output_video_path, fourcc, 30.0, (frame_width, frame_height))
 
+frame_skip = 5  # จำนวนเฟรมที่จะข้าม
+frame_count = 0  # ตัวนับเฟรม
 
 while ret:
     frame_number += 1
     ret, frame = video.read()
 
-    if ret:
+    if frame_count % frame_skip == 0:
         results[frame_number] = {}
 
         # Vehicle detector
@@ -89,6 +123,12 @@ while ret:
             if int(class_id) in vehicles and score > 0.5:
                 vehicle_class = vehicles[int(class_id)]  # Get the class name (car, bus, truck)
                 
+                # Crop the vehicle image
+                vehicle_img = frame[int(y1):int(y2), int(x1):int(x2)]
+
+                # Save the vehicle image (only once per track_id)
+                vehicle_image_path = save_vehicle_image(frame_number, track_id, vehicle_img, saved_track_ids)
+
                 # Region of interest (ROI) for license plate detection
                 roi = frame[int(y1):int(y2), int(x1):int(x2)]
 
@@ -100,35 +140,31 @@ while ret:
 
                     # Crop the plate from the region of interest
                     plate = roi[int(plate_y1):int(plate_y2), int(plate_x1):int(plate_x2)]
+                    # Save the license plate image
+                    license_plate_image_path = save_license_plate_image(track_id, plate)
 
                     # Check if the license plate is within the bottom half of the frame
                     if y2 > (frame_height * 0.5):
-                        plate_gray = cv.cvtColor(plate, cv.COLOR_BGR2GRAY)
-
-                        # Apply adaptive thresholding for better OCR
-                        plate_thresh = cv.adaptiveThreshold(plate_gray, 255, 
-                                                            cv.ADAPTIVE_THRESH_GAUSSIAN_C, 
-                                                            cv.THRESH_BINARY_INV, 21, 4)
-
+                        plate_thresh = preprocess_image(plate)
                         # OCR
                         np_text, np_score = read_license_plate(plate_thresh)
 
                         if np_text is not None:
-                            # Save detected license plate image
-                            save_license_plate_image(frame_number, track_id, plate)
 
                             # Store results
                             results[frame_number][track_id] = {
                                 'vehicle': {
                                     'class_name': vehicle_class,
                                     'bbox': [x1, y1, x2, y2],
-                                    'bbox_score': score
+                                    'bbox_score': score,
+                                    'image_path': vehicle_image_path
                                 },
                                 'license_plate': {
                                     'bbox': [plate_x1, plate_y1, plate_x2, plate_y2],
                                     'bbox_score': plate_score,
                                     'number': np_text,
-                                    'text_score': np_score
+                                    'text_score': np_score,
+                                    'image_path': license_plate_image_path  # Add the license plate image path
                                 }
                             }
                         else:
@@ -136,25 +172,28 @@ while ret:
                                 'vehicle': {
                                     'class_name': vehicle_class,
                                     'bbox': [x1, y1, x2, y2],
-                                    'bbox_score': score
+                                    'bbox_score': score,
+                                    'image_path': vehicle_image_path
                                 },
                                 'license_plate': {
                                     'bbox': [plate_x1, plate_y1, plate_x2, plate_y2],
                                     'bbox_score': plate_score,
                                     'number': None,
-                                    'text_score': None
+                                    'text_score': None,
+                                    'image_path': None  # No license plate image if not detected
                                 }
                             }
                         cv.rectangle(frame, (int(x1) + int(plate_x1), int(y1) + int(plate_y1)), 
-                                 (int(x1) + int(plate_x2), int(y1) + int(plate_y2)), (255, 0, 0), 2)
+                                     (int(x1) + int(plate_x2), int(y1) + int(plate_y2)), (255, 0, 0), 2)
                         cv.putText(frame, f'License: {np_text}', (int(x1), int(y2) + 30), 
-                               cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+                                   cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+                        
+    frame_count = 0  # เมื่อเริ่มต้นใหม่
     video_writer.write(frame)
-
 
 video_writer.release()
 # Save results to CSV
-write_csv(results, './results.csv')
+write_csv(results, './results_test.csv')
 
 # Release video capture
 video.release()
